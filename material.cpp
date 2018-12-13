0022045726558ac5f1abd0492ed2cbf1af5be0a7
Alex Li <alexyli0330@gmail.com>
2018-12-13 16:52:03 +0800

    upload class material

---
+++ material.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex_Li on 2018/12/13.
+//
+
+#include "material.h"
---
+++ material.h
@@ -0,0 +1,18 @@
+//
+// Created by Alex_Li on 2018/12/13.
+//
+
+#ifndef OPENGL_MATERIAL_H
+#define OPENGL_MATERIAL_H
+
+
+#include "texture.h"
+
+class Material
+{
+private:
+	Texture texture;
+};
+
+
+#endif //OPENGL_MATERIAL_H
