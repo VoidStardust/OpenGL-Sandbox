6a12fa35bc306f74dbce784edb481a4e23c13fc3
Alex Li <alexyli0330@gmail.com>
2018-12-13 16:52:16 +0800

    upload class point

---
+++ point.h
@@ -0,0 +1,34 @@
+//
+// Created by Alex_Li on 2018/11/27.
+//
+
+#ifndef OPENGL_POINT_H
+#define OPENGL_POINT_H
+
+class Vector;
+
+class Point
+{
+private:
+	double x = 0;
+	double y = 0;
+	double z = 0;
+
+public:
+	double getX() const;
+	void setX(double x);
+	double getY() const;
+	void setY(double y);
+	double getZ() const;
+	void setZ(double z);
+
+public:
+	Point() = default;
+	Point(double x, double y, double z);
+	void setPoint(double x, double y, double z);
+	void movePoint(double x, double y, double z);
+	void movePoint(const Vector &vector);
+};
+
+
+#endif //OPENGL_POINT_H
